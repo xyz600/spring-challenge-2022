f72c52d0f0a7a53f7c4e9abe76abcd6911d3fa80
xyz600 <xyz600600@gmail.com>
2022-04-21 23:11:38 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,130 @@
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+macro_rules! input_old {
+    (line_num: $n:expr, $($t:tt)+) => {
+        let content = ::std::io::BufRead::lines(::std::io::stdin().lock())
+            .take($n)
+            .map(|line| line.unwrap())
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        let source = content.as_str();
+        input! {
+            source = source,
+            $($t)*
+        }
+    };
+}
+
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let base_x = parse_input!(inputs[0], i32); // The corner of the map representing your base
+    let base_y = parse_input!(inputs[1], i32);
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let heroes_per_player = parse_input!(input_line, i32); // Always 3
+
+    // game loop
+    loop {
+        for i in 0..2 as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let health = parse_input!(inputs[0], i32); // Your base health
+            let mana = parse_input!(inputs[1], i32); // Ignore in the first league; Spend ten mana to cast a spell
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let entity_count = parse_input!(input_line, i32); // Amount of heros and monsters you can see
+        for i in 0..entity_count as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let id = parse_input!(inputs[0], i32); // Unique identifier
+            let type = parse_input!(inputs[1], i32); // 0=monster, 1=your hero, 2=opponent hero
+            let x = parse_input!(inputs[2], i32); // Position of this entity
+            let y = parse_input!(inputs[3], i32);
+            let shield_life = parse_input!(inputs[4], i32); // Ignore for this league; Count down until shield spell fades
+            let is_controlled = parse_input!(inputs[5], i32); // Ignore for this league; Equals 1 when this entity is under a control spell
+            let health = parse_input!(inputs[6], i32); // Remaining health of this monster
+            let vx = parse_input!(inputs[7], i32); // Trajectory of this monster
+            let vy = parse_input!(inputs[8], i32);
+            let near_base = parse_input!(inputs[9], i32); // 0=monster with no target yet, 1=monster targeting a base
+            let threat_for = parse_input!(inputs[10], i32); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+        }
+        for i in 0..heroes_per_player as usize {
+
+            // Write an action using println!("message...");
+            // To debug: eprintln!("Debug message...");
+
+
+            // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
+            println!("WAIT");
+        }
+    }
+}
